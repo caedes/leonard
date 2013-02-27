59751f23b69cf5398953fdf8d51a1eb3dc367471
Romain Laurent <romain.laurent@wopata.com>
2013-02-27 20:36:05 +0100

    Add Leonard#add_rule

--- leonard.rb
+++ leonard.rb
@@ -9,12 +9,27 @@ class Leonard
   def add_fact(fact)
     @facts << fact
   end
+
+  def add_rule(conditions, result)
+    @rules << [conditions, result]
+  end
 end
 
 l = Leonard.new
 
-l.add_fact 'enfants'
-l.add_fact 'loyer'
+l.add_fact 'no_child'
+l.add_fact 'children'
+%w(low average high).each do |amount|
+  %w(rent path wage).each do |type|
+    l.add_fact "#{amount}_#{type}"
+  end
+end
+
+l.add_rule ['no_child'], 'discount_children_0'
+l.add_rule ['children'], 'discount_children_100'
+l.add_rule ['low_wage'], 'discount_rent_200'
+l.add_rule ['average_wage'], 'discount_rent_100'
+l.add_rule ['high_wage'], 'discount_rent_0'
 
 p l.facts
 p l.rules
