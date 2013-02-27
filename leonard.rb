9f59c52548e5b5c1c18792f8f9d0384a54845ffd
Romain Laurent <romain.laurent@wopata.com>
2013-02-28 00:36:09 +0100

    Add Engine#search

--- leonard.rb
+++ leonard.rb
@@ -36,43 +36,75 @@ module Leonard
       @rules << Leonard::Rule.new(conditions, result)
     end
 
+    def has_fact?(fact)
+      @facts.map(&:name).include?(fact.class == Leonard::Fact ? fact.name : fact)
+    end
+
     def checked_rule?(rule)
       rule.conditions.sort == rule.conditions.sort & @facts.map(&:name).sort
+    end
+
+    def search(facts)
+      facts.each do |fact|
+        self.add_fact fact unless self.has_fact?(fact)
+        @rules.each do |rule|
+          if rule.has_fact?(fact) && self.checked_rule?(rule) && !@facts.map(&:name).include?(rule.result)
+            facts << rule.result
+          end
+        end
+      end
+      @facts.last
     end
   end
 end
 
 l = Leonard::Engine.new
 
-# FACTS
+# FACTS 1
 
-l.add_fact 'no_child'
+l.add_fact 'low_wage'
+l.add_fact 'rent'
 l.add_fact 'children'
-%w(low average high).each do |amount|
-  %w(rent path wage).each do |type|
-    l.add_fact "#{amount}_#{type}"
-  end
-end
+l.add_fact 'high_path'
+
+# FACTS 2
+
+# l.add_fact 'no_child'
+# l.add_fact 'rent'
+# l.add_fact 'average_wage'
+# l.add_fact 'low_path'
 
 # RULES
 
 l.add_rule ['no_child'], 'discount_children_0'
 l.add_rule ['children'], 'discount_children_100'
+l.add_rule ['low_wage'], 'discount_rent_200'
+l.add_rule ['average_wage'], 'discount_rent_100'
+l.add_rule ['high_wage'], 'discount_rent_0'
+l.add_rule ['no_rent'], 'discount_rent_0'
+l.add_rule ['low_path'], 'discount_path_0'
+l.add_rule ['discount_children_0', 'high_path'], 'discount_path_100'
+l.add_rule ['discount_rent_0', 'high_path'], 'discount_path_100'
+l.add_rule ['discount_children_100', 'discount_rent_100', 'high_path'], 'discount_path_50'
+l.add_rule ['discount_children_100', 'discount_rent_200', 'high_path'], 'discount_path_0'
+l.add_rule ['discount_children_0', 'discount_rent_0', 'discount_path_0'], 'discount_0'
+l.add_rule ['discount_children_100', 'discount_rent_0', 'discount_path_0'], 'discount_100'
+l.add_rule ['discount_children_0', 'discount_rent_100', 'discount_path_0'], 'discount_100'
+l.add_rule ['discount_children_100', 'discount_rent_100', 'discount_path_0'], 'discount_200'
+l.add_rule ['discount_children_0', 'discount_rent_200', 'discount_path_0'], 'discount_200'
+l.add_rule ['discount_children_100', 'discount_rent_200', 'discount_path_0'], 'discount_300'
+l.add_rule ['discount_children_0', 'discount_rent_0', 'discount_path_50'], 'discount_50'
+l.add_rule ['discount_children_100', 'discount_rent_0', 'discount_path_50'], 'discount_150'
+l.add_rule ['discount_children_100', 'discount_rent_100', 'discount_path_50'], 'discount_250'
+l.add_rule ['discount_children_0', 'discount_rent_200', 'discount_path_50'], 'discount_250'
+l.add_rule ['discount_children_100', 'discount_rent_200', 'discount_path_50'], 'discount_350'
+l.add_rule ['discount_children_0', 'discount_rent_0', 'discount_path_100'], 'discount_100'
+l.add_rule ['discount_children_100', 'discount_rent_0', 'discount_path_100'], 'discount_200'
+l.add_rule ['discount_children_0', 'discount_rent_100', 'discount_path_100'], 'discount_200'
+l.add_rule ['discount_children_100', 'discount_rent_100', 'discount_path_100'], 'discount_300'
+l.add_rule ['discount_children_0', 'discount_rent_200', 'discount_path_100'], 'discount_300'
+l.add_rule ['discount_children_100', 'discount_rent_200', 'discount_path_100'], 'discount_400'
 
-l.add_rule ['low_path'], 'discount_rent_0'
-l.add_rule ['average_path'], 'discount_rent_100'
-l.add_rule ['high_path'], 'discount_rent_200'
+# SEARCH
 
-l.add_rule ['children', 'high_path'], 'discount_path_50'
-
-# Rule#has_fact?
-
-p l.rules.first.has_fact? 'no_child'
-p l.rules.first.has_fact? l.facts.first
-
-# Engine#check_rule
-
-p l.checked_rule? l.rules.last
-
-p l.facts
-p l.rules
+p l.search l.facts.map(&:name)
