52b0cf77fd1a73f273a789a50213abc1e3c9ae58
Romain Laurent <romain.laurent@wopata.com>
2013-02-27 20:22:13 +0100

    Add Leonard#ad_fact

--- leonard.rb
+++ leonard.rb
@@ -5,9 +5,16 @@ class Leonard
     @facts = []
     @rules = []
   end
+
+  def add_fact(fact)
+    @facts << fact
+  end
 end
 
 l = Leonard.new
+
+l.add_fact 'enfants'
+l.add_fact 'loyer'
 
 p l.facts
 p l.rules
